c8dfe83e3b195f80a58a2d42e5d21227041c0d5c
Dima Zvashiy <bengorde80@gmail.com>
2015-11-05 22:36:52 +0200

    role dao done

--- src/main/java/com/ukmaSupport/dao/impl/RoleImpl.java
+++ src/main/java/com/ukmaSupport/dao/impl/RoleImpl.java
@@ -3,34 +3,41 @@ package com.ukmaSupport.dao.impl;
 import com.ukmaSupport.dao.interfaces.RoleDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
 
-/**
- * Created by Dima on 05.11.2015.
- */
+import java.lang.reflect.Type;
+import java.sql.*;
+
+
 public class RoleImpl implements RoleDao {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    private String GET_ROLE_BY_ID = "SELE";
+    private String GET_ROLE_BY_ID = "SELECT role FROM user_roles WHERE id = ?";
+
+    private String ADD_NEW_ROLE = "INSERT INTO user_roles (role) VALUE (?)";
+
+    private String DELETE_ROLE = "DELETE FROM user_roles WHERE id = ?";
 
     @Override
     public String getRoleById(int id) {
-        return null;//jdbcTemplate.queryForObject(GET_ROLE_BY_ID,id);
+        return jdbcTemplate.queryForObject(GET_ROLE_BY_ID, new Object[]{id}, new RowMapper<String>() {
+            @Override
+            public String mapRow(ResultSet resultSet, int i) throws SQLException {
+                return resultSet.getString("role");
+            }
+        });
     }
 
     @Override
     public void addNewRole(String role) {
-
-    }
-
-    @Override
-    public void updateRole(String role) {
-
+        jdbcTemplate.update(ADD_NEW_ROLE,new Object[]{role},new Object[]{Types.VARCHAR});
     }
 
     @Override
     public void deleteRole(int id) {
-
+        jdbcTemplate.update(DELETE_ROLE,new Object[]{id});
     }
 }
--- src/main/java/com/ukmaSupport/dao/interfaces/RoleDao.java
+++ src/main/java/com/ukmaSupport/dao/interfaces/RoleDao.java
@@ -1,15 +1,11 @@
 package com.ukmaSupport.dao.interfaces;
 
-/**
- * Created by Dima on 05.11.2015.
- */
+
 public interface RoleDao {
 
     public String getRoleById(int id);
 
     public void addNewRole(String role);
-
-    public void updateRole(String role);
 
     public void deleteRole(int id);
 
