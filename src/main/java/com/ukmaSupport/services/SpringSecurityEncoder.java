8f1be704f30632b66bf826602301c37d227f5d89
Dima Zvashiy <bengorde80@gmail.com>
2015-11-09 22:00:49 +0200

    commit

--- src/main/java/com/ukmaSupport/services/SpringSecurityEncoder.java
+++ src/main/java/com/ukmaSupport/services/SpringSecurityEncoder.java
@@ -3,9 +3,7 @@ package com.ukmaSupport.services;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by Dima on 09.11.2015.
- */
+
 
 @Service
 public class SpringSecurityEncoder implements PasswordEncoder {
