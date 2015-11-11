41e6a8e35271974ad41722991acfa36e541da0b6
Роман Цімура <Rio1995@meta.ua>
2015-11-11 18:48:55 +0200

    added new methods to AdminController

--- src/main/java/com/ukmaSupport/controllers/AdminController.java
+++ src/main/java/com/ukmaSupport/controllers/AdminController.java
@@ -1,7 +1,10 @@
 package com.ukmaSupport.controllers;
 
+import com.ukmaSupport.models.Auditorium;
+import com.ukmaSupport.models.Order;
 import com.ukmaSupport.models.User;
 import com.ukmaSupport.services.interfaces.AuditoriumService;
+import com.ukmaSupport.services.interfaces.OrderService;
 import com.ukmaSupport.services.interfaces.UserService;
 import com.ukmaSupport.services.interfaces.WorkplaceService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +28,9 @@ public class AdminController {
     @Autowired
     private WorkplaceService workplaceDao;
 
+    @Autowired
+    private OrderService orderDao;
+
     @RequestMapping(value = "/adminPage", method = RequestMethod.GET)
     public ModelAndView admin() {
         return new ModelAndView("adminPages/adminPage", "command", new User());
@@ -35,7 +41,6 @@ public class AdminController {
         if (bindingResult.hasErrors())
             return "adminPages/adminPage";
         return "result";
-
     }
 
     @RequestMapping(value = "/users", method = RequestMethod.GET)
@@ -43,5 +48,41 @@ public class AdminController {
         List<User> users = userDao.getAll();
         //model.addAttribute("users", users);
         return "adminPage/adminPageListUsers";
+    }
+
+    @RequestMapping(value = "/orders/all", method = RequestMethod.GET)
+    public String showOrders(Model model) {
+        List<Order> orders = orderDao.getAll();
+        //model.addAttribute("orders", orders);
+        return "adminPage/allOrders";
+    }
+
+    @RequestMapping(value = "/auditoriums", method = RequestMethod.GET)
+    public String showAuditoriums(Model model) {
+        List<Auditorium> auditoriums = auditoriumDao.getAll();
+        //model.addAttribute("auditoriums", auditoriums);
+        return "adminPage/auditoriums";
+    }
+
+    @RequestMapping(value = "/orders/completed", method = RequestMethod.GET)
+    public String showCompletedOrders(Model model) {
+        List<Order> orders = orderDao.getByStatus("done");
+        //model.addAttribute("orders", orders);
+        return "adminPage/orders";
+    }
+
+    @RequestMapping(value = "/orders/uncompleted", method = RequestMethod.GET)
+    public String showUncompletedOrders(Model model) {
+        List<Order> orders = orderDao.getByStatus("undone");
+        //model.addAttribute("orders", orders);
+        return "adminPage/orders";
+    }
+
+    @RequestMapping(value = "/downloadExcel", method = RequestMethod.GET)
+    public ModelAndView downloadExcel() {
+        // create some sample data
+        List<User> listUsers = userDao.getAll();
+        // return a view which will be resolved by an excel view resolver
+        return new ModelAndView("excelView", "listUsers", listUsers);
     }
 }
