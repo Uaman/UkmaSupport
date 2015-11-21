d24d5a16dcd973eb85c5c4fa3dca1b05fd2d7230
Dima Zvashiy <bengorde80@gmail.com>
2015-11-21 19:38:35 +0200

    fixed content

--- src/main/java/com/ukmaSupport/utils/OrderValidator.java
+++ src/main/java/com/ukmaSupport/utils/OrderValidator.java
@@ -38,7 +38,7 @@ public class OrderValidator implements Validator {
 
             if (order.getContent() == "") {
                 errors.rejectValue("content", "valid.order.content");
-            }else if(order.getTitle().length() < 10) errors.rejectValue("content","valid.order.shortContent");
+            }else if(order.getContent().length() <= 10) errors.rejectValue("content","valid.order.shortContent");
         }
 
     }
