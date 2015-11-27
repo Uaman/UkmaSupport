374e7f7f16d09121899dd341c90f949777a404b0
Роман Цімура <Rio1995@meta.ua>
2015-11-27 12:35:56 +0200

    errors are displayed sequentially during the registration

--- src/main/java/com/ukmaSupport/utils/RegistrationValidator.java
+++ src/main/java/com/ukmaSupport/utils/RegistrationValidator.java
@@ -26,21 +26,20 @@ public class RegistrationValidator implements Validator {
         User form = (User) obj;
         if (form.getFirstName().equals("") || form.getLastName().equals("") || form.getEmail().equals("") || form.getPassword().equals("") || form.getConfPassword().equals(""))
             errors.rejectValue("dateOfEntry", "valid.order.fillAllTheForms");
-        else {
-            if (!form.getFirstName().matches(FIO_PATTERN))
-                errors.rejectValue("firstName", "valid.firstName", "FirstName is required.");
-            if (!form.getLastName().matches(FIO_PATTERN))
-                errors.rejectValue("lastName", "valid.lastName", "LastName is required.");
-            if (!form.getEmail().matches(EMAIL_PATTERN))
-                errors.rejectValue("email", "valid.email", "Email is required.");
-            else if (userDao.getByEmail(form.getEmail()) != null)
-                errors.rejectValue("email", "valid.duplicatedEmail", "Email is required.");
-            if (form.getPassword().length() < 8)
-                errors.rejectValue("password", "valid.password.length", "Incorrect password");
-            else if (!form.getPassword().matches(PASSWORD_PATTERN))
-                errors.rejectValue("password", "valid.password2", "Incorrect password");
-            else if (!form.getPassword().equals(form.getConfPassword()))
-                errors.rejectValue("confPassword", "valid.confPasswordDiff", "Passwords are different.");
-        }
+        else if (!form.getFirstName().matches(FIO_PATTERN))
+            errors.rejectValue("firstName", "valid.firstName", "FirstName is required.");
+        else if (!form.getLastName().matches(FIO_PATTERN))
+            errors.rejectValue("lastName", "valid.lastName", "LastName is required.");
+        else if (!form.getEmail().matches(EMAIL_PATTERN))
+            errors.rejectValue("email", "valid.email", "Email is required.");
+        else if (userDao.getByEmail(form.getEmail()) != null)
+            errors.rejectValue("email", "valid.duplicatedEmail", "Email is required.");
+        else if (form.getPassword().length() < 8)
+            errors.rejectValue("password", "valid.password.length", "Incorrect password");
+        else if (!form.getPassword().matches(PASSWORD_PATTERN))
+            errors.rejectValue("password", "valid.password2", "Incorrect password");
+        else if (!form.getPassword().equals(form.getConfPassword()))
+            errors.rejectValue("confPassword", "valid.confPasswordDiff", "Passwords are different.");
+
     }
 }
