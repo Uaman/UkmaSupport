b0c9a97ecfe47c5f8bab03947064911c80f4ec97
Роман Цімура <Rio1995@meta.ua>
2015-11-27 17:05:57 +0200

    edit order fixed

--- src/main/java/com/ukmaSupport/utils/OrderValidator.java
+++ src/main/java/com/ukmaSupport/utils/OrderValidator.java
@@ -18,11 +18,11 @@ public class OrderValidator implements Validator {
     public void validate(Object o, Errors errors) {
 
         Order order = (Order) o;
-        if ((order.getTitle() == "") && (order.getContent() == "")
+        if ((order.getTitle().equals("")) && (order.getContent().equals(""))
                 && (order.getWorkplace_access_num() == null)) {
             errors.rejectValue("auditorium", "valid.order.fillAllTheForms");
         } else {
-            if (order.getTitle() == "") {
+            if (order.getTitle().equals("")) {
                 errors.rejectValue("title", "valid.order.title");
             } else if (order.getTitle().length() < 8) {
                 errors.rejectValue("title", "valid.order.shortTitle");
@@ -34,7 +34,7 @@ public class OrderValidator implements Validator {
                 errors.rejectValue("workplace_access_num", "valid.order.workspace");
             }
 
-            if (order.getContent() == "") {
+            if (order.getContent().equals("")) {
                 errors.rejectValue("content", "valid.order.content");
             } else if (order.getContent().length() < 10) {
                 errors.rejectValue("content", "valid.order.shortContent");
