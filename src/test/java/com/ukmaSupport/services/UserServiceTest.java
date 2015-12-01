6ccb99af92b985401255048073573f8020d8f9a3
Роман Цімура <Rio1995@meta.ua>
2015-12-01 20:18:48 +0200

    tests added

---
+++ src/test/java/com/ukmaSupport/services/UserServiceTest.java
@@ -0,0 +1,49 @@
+package com.ukmaSupport.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserServiceTest {
+
+
+    @Test
+    public void testGetById() throws Exception {
+
+    }
+
+    @Test
+    public void testGetResponsibleAssistant() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testSaveOrUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+
+    @Test
+    public void testGetByEmail() throws Exception {
+
+    }
+
+    @Test
+    public void testGetByRole() throws Exception {
+
+    }
+
+    @Test
+    public void testGetByStatus() throws Exception {
+
+    }
+}
