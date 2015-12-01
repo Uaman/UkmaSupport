e6e4ec4320d1be7950c88fc3f82e0c618ad650f2
Dima <d.gachok@gmail.com>
2015-12-01 21:43:42 +0200

    Test DAO mockito

---
+++ src/test/com/ukmaSupport/dao/impl/AuditoriumDaoImplTest.java
@@ -0,0 +1,43 @@
+package com.ukmaSupport.dao.impl;
+
+import com.ukmaSupport.dao.interfaces.AuditoriumDao;
+import com.ukmaSupport.dao.mapper.AuditoriumMapper;
+import com.ukmaSupport.models.Auditorium;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class AuditoriumDaoImplTest {
+
+    private static final String EXPECTED_NUMBER = "number";
+    private static final int EXPECTED_USER_ID = 1;
+    @InjectMocks
+    AuditoriumDaoImpl dao;
+
+    @Mock
+    JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void shouldSaveAuditorium(){
+        //given
+        Auditorium auditorium = new Auditorium();
+        auditorium.setNumber(EXPECTED_NUMBER);
+        auditorium.setUserId(EXPECTED_USER_ID);
+        //when
+        dao.save(auditorium);
+        //then
+        verify(jdbcTemplate).update(any(String.class), eq(EXPECTED_USER_ID) ,eq(EXPECTED_NUMBER));
+    }
+
+}
